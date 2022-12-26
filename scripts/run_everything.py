02502d4c85de65c363ce1ef8e5ddd843b796f741
cmdr2 <secondary.cmdr2@gmail.com>
2022-12-26 21:36:22 +0530

    Allow skipping models in the run everything script

--- scripts/run_everything.py
+++ scripts/run_everything.py
@@ -14,17 +14,26 @@ from sdkit.utils import save_images, log
 parser = argparse.ArgumentParser()
 parser.add_argument('--models-dir', type=str, required=True, help="Path to the directory containing all the models, with subdirs for each model-type")
 parser.add_argument('--out-dir', type=str, required=True, help="Path to the directory to save the generated images")
+parser.add_argument('--skip-models', type=str, default=None, help="Comma-separated list of model ids (without spaces) to skip")
 args = parser.parse_args()
 
 # setup
 models_db = get_models_db()
 samplers = list(default_samplers.samplers.keys()) + list(k_samplers.samplers.keys())
 
+skip_models = []
+if args.skip_models is not None:
+    skip_models = args.skip_models.split(',')
+
 # run stable diffusion tests
 sd_models = models_db['stable-diffusion']
 context = Context()
 
 for model_id, model_info in sd_models.items():
+    if model_id in skip_models:
+        print(f'skipping {model_id} as requested')
+        continue
+
     context.model_paths['stable-diffusion'] = resolve_downloaded_model_path(model_type='stable-diffusion', model_id=model_id, download_base_dir=args.models_dir)
     load_model(context, 'stable-diffusion', scan_model=False)
 
