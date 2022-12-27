bd063916348166829836db6296b1698a17e49955
cmdr2 <secondary.cmdr2@gmail.com>
2022-12-27 20:37:08 +0530

    Force full precision on cpu

--- sdkit/__init__.py
+++ sdkit/__init__.py
@@ -1,13 +1,15 @@
 from threading import local
 
 class Context(local):
+    def __init__(self) -> None:
+        self._device: str = 'cuda'
+        self._half_precision: bool = True
+
     models: dict = {}
     model_paths: dict = {}
     model_configs: dict = {}
 
-    device: str = 'cuda'
     device_name: str = None
-    half_precision: bool = True
     vram_optimizations: set = {'KEEP_FS_AND_CS_IN_CPU', 'SET_ATTENTION_STEP_TO_4'}
     '''
     Possible values:
@@ -26,3 +28,26 @@ class Context(local):
     * `'SET_ATTENTION_STEP_TO_4'`: Pretty useful! Fairly fast performance, consumes a medium amount of VRAM. For the sd-v1-4 model,
     it consumes about 1 GB more than `'KEEP_ENTIRE_MODEL_IN_CPU'`, for a much faster rendering performance.
     '''
+
+    # hacky approach, but we need to enforce full precision for some devices
+    # we also need to force full precision for these devices (haven't implemented this yet):
+    # (('nvidia' in device_name or 'geforce' in device_name) and (' 1660' in device_name or ' 1650' in device_name)) or ('Quadro T2000' in device_name)
+    @property
+    def device(self):
+        return self._device
+
+    @device.setter
+    def device(self, d):
+        self._device = d
+        if d == 'cpu':
+            from sdkit.utils import log
+            log.info('forcing full precision for device: cpu')
+            self._half_precision = False
+
+    @property
+    def half_precision(self):
+        return self._half_precision
+
+    @half_precision.setter
+    def half_precision(self, h):
+        self._half_precision = h if self._device != 'cpu' else False
