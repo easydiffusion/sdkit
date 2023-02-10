5e2092e73d990e4c99671ecab578ed42e07a8de1
Thomas Roche <thomas.a.roche@gmail.com>
2023-02-10 13:28:24 +0000

    removed dead code

--- scripts/compare_benchmarks.py
+++ scripts/compare_benchmarks.py
@@ -1,11 +1,9 @@
 import sys
 
-# import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas
+import pandas as pd
 
-stable = pandas.read_csv(sys.argv[1])
-beta = pandas.read_csv(sys.argv[2])
+stable = pd.read_csv(sys.argv[1])
+beta = pd.read_csv(sys.argv[2])
 
 del stable["vram_usage"], stable["ram_usage"]
 del beta["vram_usage"], beta["ram_usage"]
@@ -28,13 +26,3 @@ good = stable["max_vram (GB)"] > beta["max_vram (GB)"]  # & (stable['model_filen
 
 print("bad results", bad)
 print("good results", good)
-
-# combined = pandas.concat([stable, beta], axis=0, ignore_index=False)
-# combined['version'] = (len(stable)*('v2.4',) + len(beta)*('v2.5',))
-# combined.reset_index(inplace=True)
-
-# combined['index'] = combined['model_filename'].apply(lambda x: x.replace('.ckpt', '').replace('.safetensors', '')) + ',' + combined['vram_usage_level'] + ',' + combined['image_size']
-
-# sns.catplot(x='index', y='max_vram (GB)', hue='version', kind='bar', data=combined)
-# plt.xticks(rotation=90, fontsize=6)
-# plt.show()
