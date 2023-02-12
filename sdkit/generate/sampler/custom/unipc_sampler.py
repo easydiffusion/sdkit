4f91146b0bbd1a740382f98e25396d6b50a04805
Toni Schornboeck <toni.schornboeck@gmail.com>
2023-02-12 14:12:13 +0100

    fixed SD2 compatibility (hopefully)

--- sdkit/generate/sampler/custom/unipc_sampler.py
+++ sdkit/generate/sampler/custom/unipc_sampler.py
@@ -887,10 +887,13 @@ class UniPCSampler(object):
 
         ns = NoiseScheduleVP('discrete', alphas_cumprod=self.alphas_cumprod)
 
+        # SD1 is "noise", SD2 is "v"
+        model_type = "v" if self.model.parameterization == "v" else "noise"
+
         model_fn = model_wrapper(
             lambda x, t, c: self.model.apply_model(x, t, c),
             ns,
-            model_type="noise",
+            model_type=model_type,
             guidance_type="classifier-free",
             condition=conditioning,
             unconditional_condition=unconditional_conditioning,
