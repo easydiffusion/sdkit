effd7db848172391badb8782a19787081d54f2d5
Thomas Roche <thomas.a.roche@gmail.com>
2023-02-15 21:49:24 +0000

    test for save_tensor_file

--- tests/unit/utils/test_file_utils.py
+++ tests/unit/utils/test_file_utils.py
@@ -5,12 +5,12 @@ import pytest
 import safetensors.torch
 import torch
 
-from sdkit.utils.file_utils import load_tensor_file
+from sdkit.utils.file_utils import load_tensor_file, save_tensor_file
 
 
 @pytest.fixture()
 def list_fixture(faker):
-    return [random.randint(0, 50) for _ in range(faker.random_int(min=1, max=10))]
+    return [faker.random_digit_not_null() for _ in range(faker.random_digit_not_null())]
 
 
 @pytest.mark.repeat(5)
@@ -41,3 +41,29 @@ def test_load_safetensors_file(tmp_path, list_fixture):
 
     # Assert
     assert torch.equal(expected_data["tensor"], loaded_data["tensor"])
+
+
+@pytest.mark.repeat(5)
+def test_save_regular_torch_file(tmp_path, list_fixture):
+    # Arrange
+    file_path = Path(tmp_path, "test_model.pth")
+    tensor = torch.tensor(list_fixture)
+
+    # Act
+    save_tensor_file(tensor, str(file_path))
+
+    # Assert
+    assert file_path.exists()
+
+
+@pytest.mark.repeat(5)
+def test_save_safetensors_file(tmp_path, list_fixture):
+    # Arrange
+    file_path = Path(tmp_path, "test_model.safetensors")
+    tensor = {"tensor": torch.tensor(list_fixture)}
+
+    # Act
+    save_tensor_file(tensor, str(file_path))
+
+    # Assert
+    assert file_path.exists()
