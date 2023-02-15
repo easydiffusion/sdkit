23dcfd5009a09b087ff2689c36eb2233486882c6
Thomas Roche <thomas.a.roche@gmail.com>
2023-02-15 22:50:07 +0000

    testing save_images

--- tests/unit/utils/test_file_utils.py
+++ tests/unit/utils/test_file_utils.py
@@ -1,11 +1,13 @@
-import random
+import os
+import shutil
 from pathlib import Path
 
 import pytest
 import safetensors.torch
 import torch
+from PIL import Image
 
-from sdkit.utils.file_utils import load_tensor_file, save_tensor_file
+from sdkit.utils.file_utils import load_tensor_file, save_images, save_tensor_file
 
 
 @pytest.fixture()
@@ -67,3 +69,92 @@ def test_save_safetensors_file(tmp_path, list_fixture):
 
     # Assert
     assert file_path.exists()
+
+
+@pytest.fixture
+def image_dir_fixture(tmp_path):
+    # create temporary directory for images
+    dir_path = Path(tmp_path, "images")
+    dir_path.mkdir()
+    yield dir_path
+    # cleanup
+    shutil.rmtree(dir_path)
+
+
+@pytest.fixture
+def image_files_fixture(faker, image_dir_fixture):
+    # generate some sample images
+    file_names = []
+
+    for i in range(5):
+        file_name = f"test_image_{i}.jpg"
+        file_path = Path(image_dir_fixture, file_name)
+        color = tuple(int(c) for c in faker.rgb_color().split(","))
+        img = Image.new("RGB", (100, 100), color=color)
+        img.save(str(file_path))
+        file_names.append(file_name)
+
+    return file_names
+
+
+def test_save_images_with_string_file_name(image_dir_fixture, image_files_fixture):
+    # Arrange
+    images = [Image.open(image_dir_fixture / file_name) for file_name in image_files_fixture]
+    expected_file_names = [f"test_image_{i}.jpg" for i in range(len(images))]
+    expected_files = [Path(image_dir_fixture, file_name) for file_name in expected_file_names]
+
+    # Act
+    save_images(images, dir_path=str(image_dir_fixture), file_name="test_image", output_format="JPEG")
+
+    # Assert
+    for file in expected_files:
+        assert file.exists()
+
+
+def test_save_images_with_function_file_name(image_dir_fixture, image_files_fixture):
+    # Arrange
+    images = [Image.open(image_dir_fixture / file_name) for file_name in image_files_fixture]
+    expected_file_names = [f"test_image_{i}.jpg" for i in range(len(images))]
+    expected_files = [Path(image_dir_fixture, file_name) for file_name in expected_file_names]
+
+    # define the file name generating function
+    def file_name_fn(index):
+        return f"test_image_{index}.jpg"
+
+    # Act
+    save_images(images, dir_path=str(image_dir_fixture), file_name=file_name_fn, output_format="JPEG")
+
+    # Assert
+    for file in expected_files:
+        print(file, type(file), file.exists())
+        assert file.exists()
+
+
+def test_save_images_png(image_dir_fixture, image_files_fixture):
+    # Arrange
+    images = [Image.open(image_dir_fixture / file_name) for file_name in image_files_fixture]
+    expected_file_names = [f"test_image_{i}.png" for i in range(len(images))]
+    expected_files = [Path(image_dir_fixture, file_name) for file_name in expected_file_names]
+
+    # Act
+    save_images(images, dir_path=str(image_dir_fixture), file_name="test_image", output_format="PNG")
+
+    # Assert
+    for file in expected_files:
+        assert file.exists()
+
+
+def test_save_images_quality(image_dir_fixture, image_files_fixture):
+    # Arrange
+    images = [Image.open(image_dir_fixture / file_name) for file_name in image_files_fixture]
+    expected_file_names = [f"test_image_{i}.jpg" for i in range(len(images))]
+    expected_files = [Path(image_dir_fixture, file_name) for file_name in expected_file_names]
+
+    # Act
+    save_images(
+        images, dir_path=str(image_dir_fixture), file_name="test_image", output_format="JPEG", output_quality=50
+    )
+
+    # Assert
+    for file in expected_files:
+        assert file.exists()
