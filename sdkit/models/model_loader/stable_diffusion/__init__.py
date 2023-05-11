766bd22594a41ac6b17baac2760e4b391c352dd5
cmdr2 <secondary.cmdr2@gmail.com>
2023-05-11 14:47:03 +0530

    Use half precision in low vram mode

--- sdkit/models/model_loader/stable_diffusion/__init__.py
+++ sdkit/models/model_loader/stable_diffusion/__init__.py
@@ -154,6 +154,8 @@ def load_diffusers_model(context: Context, model_path, config_file_path):
     save_tensor_file(default_pipe.vae.state_dict(), os.path.join(tempfile.gettempdir(), "sd-base-vae.safetensors"))
 
     if context.vram_usage_level == "low" and context.device != "mps":
+        if context.half_precision:
+            default_pipe = default_pipe.to("cpu", torch.float16)
         default_pipe.enable_sequential_cpu_offload()
     else:
         if context.half_precision:
