96219b04846148861bd921f7f944ba626252267d
cmdr2 <secondary.cmdr2@gmail.com>
2023-05-22 16:23:31 +0530

    Formatting and remove unused variables in latent upscaler

--- sdkit/filter/latent_upscaler.py
+++ sdkit/filter/latent_upscaler.py
@@ -1,34 +1,25 @@
-import numpy as np
-from PIL import Image
 import torch
 
 from sdkit import Context
-from sdkit.utils import log
-from diffusers import StableDiffusionLatentUpscalePipeline
-
 
 
 def apply(context: Context, image, latent_upscaler_options=None, **kwargs):
-    log.info('Apply latent upscale')
     upscaler = context.models["latent_upscaler"]
 
     options = {}
     if latent_upscaler_options is not None:
-        task_data = latent_upscaler_options
         latent_upscaler_options = latent_upscaler_options.use_latent_upscaler
-        from pprint import pprint
+        options["prompt"] = latent_upscaler_options["prompt"]
+        options["negative_prompt"] = latent_upscaler_options["negative_prompt"]
 
-        options['prompt'] = latent_upscaler_options['prompt']
-        options['negative_prompt'] = latent_upscaler_options['negative_prompt']
+        options["generator"] = torch.manual_seed(int(latent_upscaler_options["seed"]))
 
-        options['generator'] = torch.manual_seed(int(latent_upscaler_options['seed']))
-
-        options['num_inference_steps'] = int(latent_upscaler_options['num_inference_steps'])
-        options['guidance_scale'] = float(latent_upscaler_options['guidance_scale'])
+        options["num_inference_steps"] = int(latent_upscaler_options["num_inference_steps"])
+        options["guidance_scale"] = float(latent_upscaler_options["guidance_scale"])
     else:
-        options['prompt'] = ""
-        options['generator'] = torch.manual_seed(172)
-        options['num_inference_steps'] = 50
-        options['guidance_scale'] = 0
+        options["prompt"] = ""
+        options["generator"] = torch.manual_seed(172)
+        options["num_inference_steps"] = 50
+        options["guidance_scale"] = 0
 
-    return upscaler( image=image, **options).images[0]
+    return upscaler(image=image, **options).images[0]
--- sdkit/models/model_loader/latent_upscaler.py
+++ sdkit/models/model_loader/latent_upscaler.py
@@ -5,17 +5,18 @@ from sdkit import Context
 from sdkit.utils import log
 
 
-
 def load_model(context: Context, **kwargs):
-    log.info('Load latent upscale model')
     dtype = torch.float16 if context.half_precision else torch.float32
-    upscaler = StableDiffusionLatentUpscalePipeline.from_pretrained("stabilityai/sd-x2-latent-upscaler", torch_dtype=dtype)
-    upscaler.to(context.device)
+    upscaler = StableDiffusionLatentUpscalePipeline.from_pretrained(
+        "stabilityai/sd-x2-latent-upscaler", torch_dtype=dtype
+    )
+    if context.vram_usage_level == "low":
+        upscaler.enable_sequential_cpu_offload()
+    else:
+        upscaler.to(context.device)
 
-    if context._vram_usage_level != "high":
-        #upscaler.enable_sequential_cpu_offload()   # GPU?
-        upscaler.enable_attention_slicing()   # GPU?
-    log.info('Loading completed')
+    if context.vram_usage_level != "high":
+        upscaler.enable_attention_slicing(1)
 
     return upscaler
 
