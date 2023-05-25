acfac95bd1c0c93e68c0abb9321e66496d027446
cmdr2 <shashank.shekhar.global@gmail.com>
2023-05-25 15:18:28 +0530

    Use the LoRA by setting the lora_alpha parameter while generating images

--- examples/008-generate-custom_lora.py
+++ examples/008-generate-custom_lora.py
@@ -6,16 +6,16 @@ from sdkit.utils import log, save_images
 context = sdkit.Context()
 context.test_diffusers = True
 
-# set the path to the model and VAE file on the disk
+# set the path to the model and LoRA file on the disk
 context.model_paths["lora"] = "D:\\path\\to\\lora.safetensors"
 context.model_paths["stable-diffusion"] = "D:\\path\\to\\model.ckpt"
 load_model(context, "stable-diffusion")
 load_model(context, "lora")
 
 # generate the image
-images = generate_images(context, prompt="Photograph of an astronaut riding a horse", seed=42, width=512, height=512)
+images = generate_images(context, prompt="Photograph of an astronaut riding a horse", seed=42, width=512, height=512, lora_alpha=0.3)
 
 # save the image
 save_images(images, dir_path="D:\\path\\to\\images\\directory")
 
-log.info("Generated images with a custom VAE!")
+log.info("Generated images with a LoRA!")
