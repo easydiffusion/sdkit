0a2ee6a1b1baa029029c836747eddcf2ed689786
cmdr2 <secondary.cmdr2@gmail.com>
2023-07-12 11:05:28 +0530

    Don't create a new tmp folder each time when running tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,11 @@ from common import fetch_test_data, OUTPUT_FOLDER
 
 
 def pytest_sessionstart(session):
-    shutil.rmtree(OUTPUT_FOLDER, ignore_errors=True)
-    os.makedirs(OUTPUT_FOLDER)
+    for filename in os.listdir(OUTPUT_FOLDER):
+        filepath = os.path.join(OUTPUT_FOLDER, filename)
+        try:
+            shutil.rmtree(filepath)
+        except:
+            os.remove(filepath)
 
     fetch_test_data()
