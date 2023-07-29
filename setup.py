8ab65995f21018dc853abe706507fba8896856c0
cmdr2 <secondary.cmdr2@gmail.com>
2023-07-29 22:02:34 +0530

    compel 2.0.1 - fixes long prompts in SDXL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
         "safetensors",
         "k-diffusion",
         "diffusers==0.19.2",
-        "compel==2.0.0",
+        "compel==2.0.1",
         "accelerate==0.18.0",
         "controlnet-aux==0.0.6",
         "invisible-watermark==0.2.0",  # required for SD XL
