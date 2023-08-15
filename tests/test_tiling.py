4f5bfa49fc9b76e3528f7323c3635c6cb3e0c145
cmdr2 <secondary.cmdr2@gmail.com>
2023-08-15 17:03:48 +0530

    Remove unused code

--- tests/test_tiling.py
+++ tests/test_tiling.py
@@ -3,7 +3,6 @@ from PIL import Image
 from sdkit import Context
 from sdkit.generate import generate_images
 from sdkit.models import load_model
-from sdkit import utils
 
 
 from common import (
@@ -15,8 +14,6 @@ EXPECTED_DIR = f"{TEST_DATA_FOLDER}/expected_images/tiling"
 
 context = None
 
-orig_has_amd = None
-
 
 def setup_module():
     global context
@@ -27,20 +24,8 @@ def setup_module():
     load_model(context, "stable-diffusion")
 
 
-"""
-def setup_function():
-    global orig_has_amd
-
-    orig_has_amd = utils.has_amd_gpu
-
-
-def teardown_function():
-    utils.has_amd_gpu = orig_has_amd
-"""
-
-
 def test_1_0__tiling_x():
-    prompt="big four-master sailing vessel, stormy waves, at night"
+    prompt = "big four-master sailing vessel, stormy waves, at night"
     image = generate_images(context, prompt, seed=42, width=512, height=512, tiling="x")[0]
 
     expected_image = Image.open(f"{EXPECTED_DIR}/1.4-tiling-x.png")
